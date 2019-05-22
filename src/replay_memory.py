ab02443b71ab007b6bf49d0dcf6742e4254f57c9
Shivam Shakti <shivam.shakti365@gmail.com>
2019-05-22 08:04:36 +0530

    Remove complex episode buffer logic and add end of episode to transition

--- src/replay_memory.py
+++ src/replay_memory.py
@@ -7,48 +7,37 @@ class ReplayMemory:
     def __init__(self, capacity):
         self.current_episode_id = 0
         self.step_id = 0
+        self.capacity = capacity
 
         self.episodes = np.empty((0, 1), dtype=np.float32)
         self.steps = np.empty((0, 1), dtype=np.float32)
-        self.states = np.empty((0, 2), dtype=np.float32)
+        self.states = np.empty((0, 4), dtype=np.float32)
         self.actions = np.empty((0, 1), dtype=np.float32)
         self.rewards = np.empty((0, 1), dtype=np.float32)
-        self.next_states = np.empty((0, 2), dtype=np.float32)
-        self.replay_memory = dict()
+        self.end = np.empty((0, 1), dtype=np.float32)
+        self.next_states = np.empty((0, 4), dtype=np.float32)
 
-
-    def add_episode(self, episode_id):
-        # Push from local memory with an id to replay memory
-        mdp = self.replay_memory[episode_id]
-        for step in range(1, len(mdp)):
-
-            state, action, reward, next_state = mdp[step]['current_state'], mdp[step]['action'], mdp[step]['reward'], mdp[step]['next_state']
-
-            self.episodes = np.append(self.episodes, np.array(episode_id, ndmin=2), axis=0)
-            self.steps = np.append(self.steps, np.array(step, ndmin=2), axis=0)
-            self.states = np.append(self.states, state.reshape(-1, 2), axis=0)
-            self.actions = np.append(self.actions, action.reshape(-1, 1), axis=0)
-            self.rewards = np.append(self.rewards, np.array(reward, ndmin=2), axis=0)
-            self.next_states = np.append(self.next_states, next_state.reshape(-1, 2), axis=0)
-
-    def add_sample(self, episode_id, transition):
+    def add_sample(self, episode_id, mdp):
         # create a local memory for each episode with metadata information
-        step = len(self.replay_memory[episode_id])
-        self.replay_memory[episode_id][step] = transition
-    """
-    def terminate_episode(self):
-        # Increment ID counters
-        self.current_episode_id += 1
         self.step_id += 1
+        state, action, reward, next_state, end = mdp['current_state'], mdp['action'], mdp['reward'], mdp['next_state'], mdp['end']
 
-        # Reset local memory
-        self.episodes = []
-        self.steps = []
-        self.states = []
-        self.actions = []
-        self.rewards = []
-        self.next_states = []
-    """
+        if type(action) is not list:
+            action = np.array(action)
+
+        self.episodes = np.append(self.episodes[-self.capacity:],
+                                  np.array(self.current_episode_id, ndmin=2), axis=0)
+        self.steps = np.append(self.steps[-self.capacity:],
+                               np.array(self.step_id, ndmin=2), axis=0)
+        self.states = np.append(self.states[-self.capacity:],
+                                state.reshape(-1, 4), axis=0)
+        self.actions = np.append(self.actions[-self.capacity:],
+                                 action.reshape(-1, 1), axis=0)
+        self.rewards = np.append(self.rewards[-self.capacity:],
+                                 np.array(reward, ndmin=2), axis=0)
+        self.next_states = np.append(self.next_states[-self.capacity:],
+                                     next_state.reshape(-1, 4), axis=0)
+        self.end = np.append(self.end[-self.capacity:], np.array(end, ndmin=2), axis=0)
 
     def fetch_sample(self, num_samples=None):
 
@@ -60,12 +49,13 @@ class ReplayMemory:
         actions = self.actions[idx]
         rewards = self.rewards[idx]
         next_states = self.next_states[idx]
-        return states, actions, rewards, next_states
+        end = self.end[idx]
+        return states, actions, rewards, next_states, end
 
     def save_memory(self):
         pass
 
     def create_episode(self):
         self.current_episode_id += 1
-        self.replay_memory[self.current_episode_id] = defaultdict(list)
+        self.step_id = 0
         return self.current_episode_id
